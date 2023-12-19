8093be45aae020df286c2b40369ee6dccbd04d54
TeresaTeng <55872812+Teresa-Teng@users.noreply.github.com>
2023-12-19 20:50:08 +0800

    Update daily_arxiv.py

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -108,8 +108,7 @@ if __name__ == "__main__":
         # you can add the categories in cats "eess":["eess.SP"],
         cats = {
             "eess":["eess.SP"],    
-            "cs":["cs.RO"]
-    }
+            "cs":["cs.RO"] }
         data = get_daily_code(day,cats)
         data_all.append(data)
     update_daily_json("daily.json",data_all)
